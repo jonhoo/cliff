8ba3ae166409f580acc93a9ca8fb73773f2e5d98
Jon Gjengset <jon@thesquareplanet.com>
2020-07-31 09:16:27 -0400

    Add left filling option

--- src/exponential.rs
+++ src/exponential.rs
@@ -6,10 +6,13 @@ use super::CliffSearch;
 #[derive(Debug, Clone)]
 pub struct ExponentialCliffSearcher {
     max_in: core::ops::Range<usize>,
+    start: usize,
+    prev_min: usize,
     last: Option<usize>,
     fidelity: usize,
     overloaded: bool,
     done: bool,
+    fill_left: bool,
 }
 
 impl ExponentialCliffSearcher {
@@ -24,10 +27,13 @@ impl ExponentialCliffSearcher {
     pub fn until(start: usize, min_width: usize) -> Self {
         Self {
             max_in: start..usize::max_value(),
+            start,
+            prev_min: start,
             fidelity: min_width,
             last: None,
             overloaded: false,
             done: false,
+            fill_left: false,
         }
     }
 
@@ -50,6 +56,27 @@ impl ExponentialCliffSearcher {
     pub fn estimate(&self) -> core::ops::Range<usize> {
         self.max_in.clone()
     }
+
+    /// Ensure that samples are taken just before the cliff.
+    ///
+    /// If the system under test supports, say, eight million operations per second, and searches
+    /// starting at 1M. The searcher will first sample 1M, 2M, 4M, 8M, 16M. When it detects that
+    /// 16M is overloaded, it'll perform a binary search from the "right", running 12M, 10M, and 9M
+    /// before ultimately deciding that 8M is the lower bound for the cliff.
+    ///
+    /// This evaluation is correct, but may yield strange-looking results when plotting as the
+    /// system is likely highly loaded at 8M. In particular, a straight line drawn from the 4M
+    /// sample to the 8M sample may yield a jarring visual image, and will make it hard to see what
+    /// happens in the system leading up to its capacity.
+    ///
+    /// This method makes the searcher "fill in" extra samples to the left of the lower bound when
+    /// necessary. In the case above, after finding that 8M is the lower bound, this filling would
+    /// also sample 6M and 7M.
+    ///
+    /// Filling also respects the minimum search range width if specified with [`until`].
+    pub fn fill_left(&mut self) {
+        self.fill_left = true;
+    }
 }
 
 impl CliffSearch for ExponentialCliffSearcher {
@@ -66,6 +93,20 @@ impl Iterator for ExponentialCliffSearcher {
     type Item = usize;
     fn next(&mut self) -> Option<Self::Item> {
         if self.done {
+            if self.fill_left {
+                // we've found the range in which the cliff lies: self.max_in
+                // but the user has requested that we also "fill the curve" up to the min
+                // by sampling some data points leading up to the cliff as well
+                let diff = self.max_in.start - self.prev_min;
+                if diff > self.fidelity {
+                    // now just binary search between prev_min and max_in.start
+                    let next = self.prev_min + diff / 2;
+                    self.prev_min = next;
+                    return Some(next);
+                } else {
+                    self.fill_left = false;
+                }
+            }
             return None;
         }
 
@@ -76,6 +117,7 @@ impl Iterator for ExponentialCliffSearcher {
                 self.overloaded = false;
             } else {
                 // the last thing succeeded, so that increases the lower limit
+                self.prev_min = self.max_in.start;
                 self.max_in.start = *last;
             }
 
@@ -93,7 +135,8 @@ impl Iterator for ExponentialCliffSearcher {
                 Some(next)
             } else {
                 self.done = true;
-                None
+                // normally just None, but may be Some with filling
+                return self.next();
             }
         } else {
             self.last = Some(self.max_in.start);
@@ -149,6 +192,34 @@ fn search_from_until() {
     assert_eq!(scale.next(), None);
     // and the estimate is still the same
     assert_eq!(scale.estimate(), 4000..5000);
+}
+
+#[test]
+fn fill_search() {
+    let mut scale = ExponentialCliffSearcher::until(500, 500);
+    scale.fill_left();
+    assert_eq!(scale.next(), Some(500));
+    assert_eq!(scale.next(), Some(1000));
+    assert_eq!(scale.next(), Some(2000));
+    assert_eq!(scale.next(), Some(4000));
+    assert_eq!(scale.next(), Some(8000));
+    scale.overloaded();
+    assert_eq!(scale.next(), Some(6000));
+    scale.overloaded();
+    assert_eq!(scale.next(), Some(5000));
+    scale.overloaded();
+    assert_eq!(scale.next(), Some(4500));
+    scale.overloaded();
+
+    // since filling is enabled, we'll also sample a few
+    // points just _before_ the highest known-good target.
+    assert_eq!(scale.next(), Some(3000));
+    assert_eq!(scale.next(), Some(3500));
+
+    // and then we should be done
+    assert_eq!(scale.next(), None);
+    // check that it continues to be terminated
+    assert_eq!(scale.next(), None);
 }
 
 #[test]
